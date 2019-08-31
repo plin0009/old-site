50e9e172c3ba8a3d83654cf0318b4ce528a44f44
plin0009 <plin0009@gmail.com>
2019-08-31 18:39:50 -0400

    I hate typos.

--- gatsby/src/pages/404.js
+++ gatsby/src/pages/404.js
@@ -10,7 +10,7 @@ const NotFoundPage = () => (
       <div className="hero-body">
         <div className="container has-text-centered">
           <h1 className="title is-1">Sorry!</h1>
-          <h2 className="subtitle">I can't find what you're looking.</h2>
+          <h2 className="subtitle">I can't find what you're looking for.</h2>
           <div className="buttons are-large is-centered">
             <a href="../" className="button is-primary is-rounded">Go back?</a>
           </div>
