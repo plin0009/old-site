55546c8a3c6a2682f9e0ea8b7fd44d03f2281cc0
plin0009 <plin0009@gmail.com>
2019-09-04 23:31:45 -0400

    Update works

--- gatsby/src/data/works.js
+++ gatsby/src/data/works.js
@@ -3,20 +3,20 @@ const works = [
         title: 'funlink',
         imageName: 'funlink',
         description: `A small gaming lobby platform using socket.io. A personal project in progress.`,
-        tags: ['back end', 'node', 'express', 'socket'],
-        link: 'https://github.com/plin0009/funlink'
+        tags: ['front end', 'back end', 'node', 'react', 'express', 'socket'],
+        link: 'https://github.com/plin0009/funlink-new'
     },
     {
         title: 'Elements',
         imageName: 'elements',
-        description: `A simple page to convert your text into chemistry elements' symbols!`,
-        tags: ['front end'],
+        description: `A simple page to convert your text into chemistry elements' symbols.`,
+        tags: ['front end', 'html', 'css', 'js'],
         link: 'https://github.com/plin0009/elements'
     },
     {
         title: 'Personal Website',
         imageName: 'website',
-        description: `A page to tell the world about me. In progress.`,
+        description: `A page to tell the world about me. Nearly polished!`,
         tags: ['front end', 'gatsby', 'react', 'bulma'],
         link: 'https://github.com/plin0009/plin0009.github.io'
     }
